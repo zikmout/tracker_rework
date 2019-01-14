8ee850a237ec28bc1846f604c77c82f599672100
Simon Sicard <sicsim64@gmail.com>
2019-01-14 16:06:19 +0100

    starting new view to create multiple crawling tasks

--- pintell/views/crawl.py
+++ pintell/views/crawl.py
@@ -1,6 +1,6 @@
 import tornado
 from pintell.views.base import BaseView
-from pintell.utils import flash_message, login_required
+from pintell.utils import flash_message, login_required, get_url_from_id
 from pintell.workers.download_worker import download_website
 
 
@@ -15,3 +15,15 @@ class UserProjectCrawlView(BaseView):
             self.redirect('/api/v1/users/{}/projects_manage'.format(self.session['username']))
         else:
             self.render('projects/crawl.html', units=units)
+
+class UserCrawlsCreate(BaseView):
+    @login_required
+    def post(self, username, projectname):
+        args = { k: self.get_argument(k) for k in self.request.arguments }
+        print('post args = {}'.format(args))
+        # if box is checked, variable comes in like { "gridCheck": "on" }
+        checked = False
+        for _ in list(args):
+            if 'selectedCheck' in _:
+                uid = _.replace('selectedCheck', '')
+                print('Selected uid to crawl : {}'.format(get_url_from_id(self.session['units'], uid)))

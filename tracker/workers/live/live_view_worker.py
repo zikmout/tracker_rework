e50ceb58fbe0623273836b32bfeca17b2e29d53e
Simon Sicard <simon@electricity.ai>
2019-09-25 10:25:27 +0200

    k

--- tracker/workers/live/live_view_worker.py
+++ tracker/workers/live/live_view_worker.py
@@ -16,7 +16,7 @@ import tracker.core.extractor as extractor
 import re
 import pdftotext
 import gc
-import fastText
+import fasttext as fastText
 
 # Hack to load only necessary modules (pb with ml model)
 # TODO: Replace raw path with os.environ ($APP_DIR)

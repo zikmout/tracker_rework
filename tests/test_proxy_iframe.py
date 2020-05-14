fe2396344d7a703df49defeb34753c0b86246971
Simon Sicard <simon@electricity.ai>
2020-05-14 08:53:33 +0200

    on mouse out ok

--- tests/test_proxy_iframe.py
+++ tests/test_proxy_iframe.py
@@ -6,8 +6,8 @@ import sys
 import requests
 
 
-# hostname = 'en.wikipedia.org'
-hostname = 'www.lequipe.fr'
+hostname = 'en.wikipedia.org'
+# hostname = 'www.lequipe.fr'
 
 
 def merge_two_dicts(x, y):
@@ -49,7 +49,10 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
                     
                     # html_injected = "<body onload=\"alert(\'toto\');\">"
                     # html_injected = "<body onload=\"document.body.style.backgroundColor = 'red';\">"
-                    html_injected = "<body onload=\"document.addEventListener('mouseover', function (event) {event.target.style.outline = '#f00 solid 2px';}, false);\">"
+                    html_injected = """
+                    <body onload=\"var outlineStyle='';document.addEventListener('mouseover', function (event) {outlineStyle=event.target.style.outline; event.target.style.outline = '#f00 solid 2px';}, false);
+                    document.addEventListener('mouseout', function (event) {event.target.style.outline = outlineStyle;}, false);\">
+                    """
                     # html_injected = """
                     # <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
                     # <script>
@@ -75,7 +78,10 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
                     
                     # html_injected = "<body onload=\"alert(\'toto\');\" "
                     # html_injected = "<body onload=\"document.body.style.backgroundColor = 'red';\" "
-                    html_injected = html_injected = "<body onload=\"document.addEventListener('mouseover', function (event) {event.target.style.outline = '#f00 solid 2px';}, false);\" "
+                    html_injected = html_injected = """
+                    <body onload=\"var outlineStyle='';document.addEventListener('mouseover', function (event) {outlineStyle=event.target.style.outline; event.target.style.outline = '#f00 solid 2px';}, false);
+                    document.addEventListener('mouseout', function (event) {event.target.style.outline = outlineStyle;}, false);\" 
+                    """
                     # html_injected = """
                     # <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
                     # <script>
